72e9fc86e40ef222e27a589d3decd00101b70ccd
Harmeet Singh <harmeetchahal9891@gmail.com>
2023-08-06 01:41:05 +0530

    .

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use('/posts', postRoutes);
 
 
 const CONNECTION_URL = 'mongodb+srv://orangefruit:orangefruit@cluster0.gpmgs.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
